afa7aa74b82528c195433ce856852598ec4d0abf
threemay <254363807@qq.com>
2021-10-25 21:32:17 +1100

    1

---
+++ add.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# the first parameter should be the master branch name
+# the first parameter should be the feature branch name
+
+# this check is checking the head main branch commit is contained 
+# in feature branch 
+# if no contain, exit with 1 code
+
+git add .
+git commit -m "1"
+git push origin main
